52451b51c427c89489dcb055db3ce3dee7d4b08e
Yurii Datsenko <root@localhost.localdomain>
2021-01-28 01:14:51 -0500

    Rebuild all parts of project

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.9.1
+FROM python:3.9.1-alpine
 WORKDIR /app
 COPY . . 
 RUN pip3 install -r requirements.txt
--- run_script.sh
+++ run_script.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
-echo "----------Install Python-----------------"
-package=python3
-	apt-get update
-	apt-get upgrade -y
-	apt-get install $package -y;
-	echo "$package is installed."
-	
-echo $'\n'
-
 echo "------Install Ansible------"
 package=ansible
-	apt-add-repository ppa:ansible/ansible
-	apt-get update -y
-	apt-get upgrade -y
-	apt-get install $package -y;
+        sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+        sudo yum install --enablerepo epel-playground ansible
 	echo "$package is installed."
 
 
@@ -22,35 +11,10 @@ echo $'\n'
 
 echo "------Install Docker------"
 package=docker
-	apt-get update -y
-	apt-get upgrade -y
-    
-	#Docker dependencies
-	
-	apt-get install \
-	apt-transport-https \
-	ca-certificates \
-	curl \
-	gnupg-agent \
-	
-	#Docker GPG key
-	
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-	
-	apt-get update -y
-	apt-get install $package -y
-	systemctl start $package -y
-	systemctl enable $package -y;
-	echo "$package is installed."
+        curl -fsSL https://get.docker.com -o install_docker.sh
+        sudo sh install_docker.sh
+        sudo usermod -aG docker root
 
-
-echo $'\n'
-
-echo "------Build Docker image------"
-	
-	#Create Docker image from Dockerfile
-	docker build -t local/app.py:prod .
-	
 echo $'\n'
 
 echo "------Run Ansible playbook------"
